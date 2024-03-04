8627ee8ed262d826007d1d62cfa92d18c1d30b22
Malcolm Nixon <Malcolm.nixon@gmail.com>
2024-03-03 23:21:26 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# GodotXROpenXRTracker
- Godot XR Tracker for OpenXR
+# Godot XR OpenXR Tracker
+
+This repository contains a demo project to evaluate the Godot-OpenXR-Vendors hand, body, and face tracking.
+
+There are two main scenes which can be used for testing:
+
+- `body_demo.tscn` tests body/face tracking
+- `hand_demo.tscn` tests hand tracking
+
+The left controller AX/BY buttons adjust the Skeleton3D motion_scale property of the bodies or hands.
+The right controller AX/BY buttons adjust the XRServer world_scale property of the environment.
+
+Note that in the hands demo, touching the controller buttons will cause OpenXR to switch to controller-mode temporarily and hide the hands. Putting the controllers down for a few seconds will restore hand-tracking and show the hands again.
---
+++ hand_demo.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+
+var skeleton_scale := 1.0
+
+
+func _ready():
+	var xr_interface := XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		get_viewport().use_xr = true
+
+
+func _on_left_controller_button_pressed(name: String) -> void:
+	match name:
+		"by_button":
+			skeleton_scale = clampf(skeleton_scale + 0.1, 0.1, 2.0)
+			%"LeftHandHumanoid/LeftHandHumanoid/Skeleton3D".motion_scale = skeleton_scale
+			%"RightHandHumanoid/RightHandHumanoid/Skeleton3D".motion_scale = skeleton_scale
+
+		"ax_button":
+			skeleton_scale = clampf(skeleton_scale - 0.1, 0.1, 2.0)
+			%"LeftHandHumanoid/LeftHandHumanoid/Skeleton3D".motion_scale = skeleton_scale
+			%"RightHandHumanoid/RightHandHumanoid/Skeleton3D".motion_scale = skeleton_scale
+
+
+func _on_right_controller_button_pressed(name: String) -> void:
+	match name:
+		"by_button":
+			XRServer.world_scale = clampf(XRServer.world_scale + 0.1, 0.1, 2.0)
+
+		"ax_button":
+			XRServer.world_scale = clampf(XRServer.world_scale - 0.1, 0.1, 2.0)
